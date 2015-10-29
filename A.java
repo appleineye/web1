67e31c7f17f14bf95194a34b91060e0257724c0c
shiwei.zhu@mail.rakuten.com <shiwei.zhu@mail.rakuten.com>
2015-10-30 06:34:00 +0800

    "Added checkout page"

--- A.java
+++ A.java
@@ -1,4 +1,26 @@
 public class A {
+
+static class Solution {
+    int solution(int X, int Y, int[] A) {
+        int N = A.length;
+        int result = -1;
+        int nX = 0;
+        int nY = 0;
+        for (int i = 0; i < N; i++) {
+            if (A[i] == X){
+                nX += 1;
+                System.out.println("A"+i+"="+A[i]+"-X-"+nX);
+            }if (A[i] == Y){
+                nY += 1;
+                System.out.println("A"+i+"="+A[i]+"-Y-"+nY);
+            }if (nX !=0 && nY != 0 && nX == nY){
+                result = i;
+                System.out.println("nX:"+nX+";nY:"+nY+";result:"+result);
+            }
+        }
+        return result;
+    }
+}
 	
 	public A(){
 		System.out.print("A");
@@ -14,5 +36,9 @@ public class A {
 
     public static final void main(String[] args) {
     	A a = new A();
+        int[] b = {6,42,11,7,1,42};
+        A.Solution s = new Solution();
+        System.out.println();
+        System.out.println("final result="+s.solution(42,0,b));
     }
 }
